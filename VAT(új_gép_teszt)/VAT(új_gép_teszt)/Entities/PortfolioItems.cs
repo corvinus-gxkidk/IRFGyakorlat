721e1dfa6c382dd5c1707cc078dcb6d4cc5c3db3
Krisztián Csürök <krisztian.csurok@stud.uni-corvinus.hu>
2021-10-22 23:36:23 +0200

    Add portfolio

---
+++ VAT(új_gép_teszt)/VAT(új_gép_teszt)/Entities/PortfolioItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VAT_új_gép_teszt_.Entities
+{
+    class PortfolioItems
+    {
+        public string Index { get; set; }
+
+        public decimal Volume { get; set; }
+    }
+}
--- VAT(új_gép_teszt)/VAT(új_gép_teszt)/Form1.cs
+++ VAT(új_gép_teszt)/VAT(új_gép_teszt)/Form1.cs
@@ -7,15 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VAT_új_gép_teszt_.Entities;
 
 namespace VAT_új_gép_teszt_
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        List<PortfolioItems> Portfolio = new List<PortfolioItems>();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
 
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItems() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItems() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItems() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
