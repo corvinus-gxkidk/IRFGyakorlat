eadbbd9381a774ecf4c0dc260681f69d80a92e31
Krisztián Csürök <krisztian.csurok@stud.uni-corvinus.hu>
2021-10-29 10:16:32 +0200

    Add Folder, add class

--- MNB/MNB/Form1.cs
+++ MNB/MNB/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using MNB.Entities;
+using MNB.ServiceReference1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,23 @@ namespace MNB
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
+            Consume();
+            dataGridView1.DataSource = Rates;
+        }
+        void Consume()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate= "2020-06-30";
+            var response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+            File.WriteAllText("export.xml", result);
         }
     }
 }
