c703d920a798748ebfb7a1e91c34e15b0fa58386
Krisztián Csürök <krisztian.csurok@stud.uni-corvinus.hu>
2021-11-12 23:22:15 +0100

    Ball add, ball move

--- Tasks8/Tasks8/Form1.cs
+++ Tasks8/Tasks8/Form1.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tasks8.Entities;
 
 namespace Tasks8
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+        int maxPosition;
+
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+            
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            mainPanel.Controls.Add(ball);
+            ball.Left = -ball.Width;
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
