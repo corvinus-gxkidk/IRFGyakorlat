148d601b15720e33993dba57e40ccd74128db090
Krisztián Csürök <krisztian.csurok@stud.uni-corvinus.hu>
2021-11-26 10:57:02 +0100

    TestCase

--- UnitTestExample/UnitTestExample.text/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.text/AccountControllerTestFixture.cs
@@ -53,6 +53,12 @@ namespace UnitTestExample.text
             Assert.AreEqual(expectedResult, result);
 
         }
+        [
+            Test,
+            TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+            ]
+
         public void TestRegisterHappyPath(string email, string password)
         {
             // Arrange
