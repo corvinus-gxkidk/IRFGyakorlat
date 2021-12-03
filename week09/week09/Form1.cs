3ea30e086704923ec16f5e36d330e48b2d6bc708
Krisztián Csürök <krisztian.csurok@stud.uni-corvinus.hu>
2021-12-03 10:15:00 +0100

    List population

--- week09/week09/Form1.cs
+++ week09/week09/Form1.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week09.Entities;
 
 namespace week09
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
         public Form1()
         {
             InitializeComponent();
+
+            Population = GetPopulation(@"F:\Temp\nép-teszt.csv");
+
+        }
+        public List<Person> GetPopulation(string csvPath)
+        {
+            List<Person> population = new List<Person>();
+
+                return population;
         }
     }
 }
