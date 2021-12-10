84ed9dcba14efb6e8e4fc3cef53b741ad4bb2b41
Krisztián Csürök <krisztian.csurok@stud.uni-corvinus.hu>
2021-12-10 10:59:13 +0100

    evolution added

--- EvolutionExample/EvolutionExample/Form1.cs
+++ EvolutionExample/EvolutionExample/Form1.cs
@@ -15,6 +15,8 @@ namespace EvolutionExample
     {
         GameController gc = new GameController();
         GameArea ga;
+        Brain winnerBrain = null;
+
 
         int populationSize = 100;
         int nbrOfSteps = 10;
@@ -39,14 +41,54 @@ namespace EvolutionExample
             }
             gc.Start();
 
+            
+
         }
 
         private void Gc_GameOver(object sender)
         {
             generation++;
-            label1.Text = string.Format(
-                "{0}. generáció",
-                generation);
+            label1.Text = generation.ToString() + ". generáció";
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            var winners = from p in topPerformers where p.IsWinner select p;
+
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
         }
     }
 }
